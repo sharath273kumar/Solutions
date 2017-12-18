0c3006a2ec874f02d9cfcb63da68323e647129e9
Sharath Kumar <sharath273kumar@gmail.com>
2017-12-18 22:41:11 +0530

    Update JobSequence.java

--- src/home11/JobSequence.java
+++ src/home11/JobSequence.java
@@ -1,3 +1,42 @@
+/*
+QUESTION:http://www.geeksforgeeks.org/job-sequencing-problem-set-1-greedy-algorithm/
+Job Sequencing Problem | Set 1 (Greedy Algorithm)
+2.9
+Given an array of jobs where every job has a deadline and associated profit if the job is finished before the deadline. It is also given that every job takes single unit of time, so the minimum possible deadline for any job is 1. How to maximize total profit if only one job can be scheduled at a time.
+
+Examples:
+
+Input: Four Jobs with following deadlines and profits
+  JobID    Deadline      Profit
+    a        4            20   
+    b        1            10
+    c        1            40  
+    d        1            30
+Output: Following is maximum profit sequence of jobs
+        c, a   
+
+
+Input:  Five Jobs with following deadlines and profits
+   JobID     Deadline     Profit
+     a         2           100
+     b         1           19
+     c         2           27
+     d         1           25
+     e         3           15
+Output: Following is maximum profit sequence of jobs
+        c, a, e
+Recommended: Please solve it on “PRACTICE ” first, before moving on to the solution.
+A Simple Solution is to generate all subsets of given set of jobs and check individual subset for feasibility of jobs in that subset. Keep track of maximum profit among all feasible subsets. The time complexity of this solution is exponential.
+
+This is a standard Greedy Algorithm problem. Following is algorithm.
+
+1) Sort all jobs in decreasing order of profit.
+2) Initialize the result sequence as first job in sorted jobs.
+3) Do following for remaining n-1 jobs
+.......a) If the current job can fit in the current result sequence 
+          without missing the deadline, add current job to the result.
+          Else ignore the current job.
+*/
 package home11;
 //		My Code
 class Job{
